5c9b8f4fd72fa27784403ac34af871fab3036812
svartse <killbillbeatrixx@gmail.com>
2023-07-03 02:02:59 +0300

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Streanime from './pages/Streanime';
 
 export default function App(){
-  return <div>App</div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/login" element={<Login/>}/>
+        <Route exact path="/signup" element={<Signup/>}/>
+        <Route exact path="/" element={<Streanime/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
